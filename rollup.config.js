12253da6ae7c6b2a14c4f2a1e70e07383257b898
Nana Adjei Manu <claeusdev@gmail.com>
2020-06-14 00:44:02 +0000

    add Ring component

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-
+import { string } from 'rollup-plugin-string'
 const production = !process.env.ROLLUP_WATCH
 
 const dist = 'dist'
@@ -32,6 +32,9 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**'
+    }),
+    string({
+      include: '**/*.css'
     }),
     production && terser()
   ]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from 'react'
-
-const Spinner = <h1>Spinning</h1>
-
-export default Spinner
+export * from './Ring'
